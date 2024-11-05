88111a4eaca31111b090edd3f19a8d0497e04961
Kimpembie2 <biello.gabriele@iisgalvanimi.edu.it>
2024-11-05 09:51:22 +0000

    prep_A

---
+++ prep_A.py
@@ -0,0 +1,13 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password=""
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("CREATE DATABASE IF NOT EXISTS Animali")
+mycursor.execute("Use Animali")
+mycursor.execute("CREATE TABLE IF NOT EXISTS Mammiferi (id INT PRIMARY KEY AUTO_INCREMENT, nome VARCHAR(255), razza VARCHAR(255))")
